73cec3085581267b78201e0bfd59b45b1d9203f4
RichardErkhov <104026872+RichardErkhov@users.noreply.github.com>
2022-07-29 15:22:30 +0300

    smallest code that I could get

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-import string
-import random
-
-
-## characters to generate password from
-characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
-
-def generate_random_password():
-	## length of password from the user
-	length = int(input("Enter password length: "))
-
-	## shuffling the characters
-	random.shuffle(characters)
-	
-	## picking random characters from the list
-	password = []
-	for i in range(length):
-		password.append(random.choice(characters))
-
-	## shuffling the resultant password
-	random.shuffle(password)
-
-	## converting the list to string
-	## printing the list
-	print("".join(password))
-
-
-
-## invoking the function
-generate_random_password()
+import string as s
+import random as r
+c=list(s.ascii_letters+s.digits+"!@#$%^&*()")
+r.shuffle(c)
+p = [r.choice(c) for i in range(int(input("length: ")))]
+r.shuffle(p)
+print("".join(p))
